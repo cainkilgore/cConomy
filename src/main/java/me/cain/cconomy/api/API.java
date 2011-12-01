4451348a317e80f45d28fa99c30c0d7744c05df1
Cain <cain.donaghey@live.co.uk>
2011-12-01 20:17:09 +0000

    Added some more API Methods.

--- src/main/java/me/cain/cconomy/api/API.java
+++ src/main/java/me/cain/cconomy/api/API.java
@@ -6,6 +6,12 @@ import org.bukkit.entity.Player;
 
 public class API
 {
+	cConomy plugin;
+	
+	public API(cConomy plugin) {
+		cConomy.plugin = plugin;
+	}
+	
 	public boolean hasExactMoney(Integer amount, Player p) {
 		int balance = cConomy.plugin.getConfig().getInt("players." + p.getName() + ".balance"); // Get how much that player currently has
 		if(amount == balance) { // Check to see if the amount is the same as the player balance
@@ -14,4 +20,24 @@ public class API
 			return false; // Return false, for any else {'s in the people using the API.
 		}
 	}
+	
+	public boolean hasNotEnoughMoney(Integer amount, Player p) {
+		int balance = cConomy.plugin.getConfig().getInt("players." + p.getName() + ".balance"); // Get how much that player currently has
+		if(balance < amount) { // Check to see if the amount is below the player balance
+			return true; // Return true.
+		} else {
+			return false; // Return false.
+		}
+	}
+	
+	public boolean hasAboveMoney(Integer amount, Player p) {
+		int balance = cConomy.plugin.getConfig().getInt("players." + p.getName() + ".balance"); // Get how much that player has
+		if(balance > amount) { // I'll stop from here, comments are getting boring.
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
 }
